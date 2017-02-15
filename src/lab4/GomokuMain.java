0fee53a6ca8d3badf5f50179a32574e57a0d744e
hugow <hugow@DESKTOP-J9R1RO0>
2017-02-15 15:03:17 +0100

    asd

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -1,13 +1,15 @@
 package lab4;
+
 import lab4.client.*;
 import lab4.gui.*;
 
-public class GomokuMain {
-	private static int port = 4500;	// The default port number is 4500.
 
-	public static void main(String[] argv) {
-		if (argv.length == 1) {
-			int tempPort = Integer.parseInt(argv[0]);	// Problem if the argument is not a number.
+public class GomokuMain {
+	private static int port = 4500;
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			int tempPort = Integer.parseInt(args[0]);	// Problem if the argument is not a number.
 			
 			if (tempPort > 0) {	// A port number needs to be a positive integer.
 				port = tempPort;
@@ -15,6 +17,7 @@ public class GomokuMain {
 		
 		GomokuClient client = new GomokuClient(port);
 		GomokuGameState gameState = new GomokuGameState(client);
-		GomokuGUI gui = new GomokuGUI(gameState, client);
+		GomokuGUI GUI = new GomokuGUI(gameState, client);
+		}
 	}
 }
